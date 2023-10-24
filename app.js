b5c32739d6121393cf7da5b6bbc100b7633cf4dc
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-10-23 17:45:52 -0700

    first comit

---
+++ app.js
@@ -0,0 +1,63 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var cors = require("cors");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+var multer = require("multer");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser());
+
+const upload = multer({ dest: "uploads/" });
+
+app.post("/upload", upload.single("file"), function (req, res, next) {
+  console.log("upload", req);
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  // console.log(req.file);
+  res.json({ message: "success" });
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
